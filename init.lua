5788b710a549efc1ef93b0ee2d123c2c248cf9c2
Eduardo Santiago <eduardossantiago@gmail.com>
2024-06-15 16:06:43 -0500

    extra config

--- init.lua
+++ init.lua
@@ -12,8 +12,19 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local opts = {}
 
 require("vim-options")
 require("lazy").setup("plugins")
 
+-- <leader>c open this configuration file
+vim.cmd('nmap <leader>c :e ~/.config/nvim/init.lua<CR>')
+
+
+-- sync system clipboard with vim clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- search ignoring case
+vim.opt.ignorecase = true
+
+-- disable "ignorecase" option if the search pattern contains upper case characters
+vim.opt.smartcase = true
